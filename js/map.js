a717b3f180955e6ef795d866350c82256517a994
천상의나무 <ajb8533296@gmail.com>
2021-08-11 14:48:31 +0900

    a

---
+++ js/map.js
@@ -0,0 +1,8 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); 
