accab194c980b6fda5039db62a8ee92bbc1bc210
easycastle <sean34418290@gmail.com>
2021-08-13 06:23:24 +0900

    feat : save sml json

--- js/xml-js.js
+++ js/xml-js.js
@@ -1,7 +1,11 @@
 const convert = require('xml-js');
 const request = require('request');
+const fs = require('fs')
 
 var requestUrl = `http://api.data.go.kr/openapi/tn_pubr_public_free_mlsv_api?serviceKey=w4WgRe0atRPd8sTRA1ca%2BWFFWFmvzG7mwOKRIoNtnUucJyKMjEzCfngxtf9BZaE5SYgG849atzzWO1L7mOyi5g%3D%3D&type=xml&numOfRows=100000`
+var xmlFile = 'xml_data.xml'
+var jsonFile = 'json_data.json'
+
 
 request.get(requestUrl, (err,res,body) =>{
     if(err){
@@ -12,8 +16,13 @@ request.get(requestUrl, (err,res,body) =>{
             var result = body // 기존 xml 파일
             var xmlToJson = convert.xml2json(result, {compact: true, spaces: 4}); // json 변환 결과
 
-            console.log(`${result}`)
-            console.log(`${xmlToJson}`)
+            fs.writeFile(xmlFile, result, 'utf8', function(error){
+                console.log('complete write xml')
+            });
+
+            fs.writeFile(jsonFile, xmlToJson, 'utf8', function(error){
+                console.log('complete write json')
+            });
         }
     }
 })
