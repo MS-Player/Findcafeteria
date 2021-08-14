0c830f2af1670250e42310ad7407543952560d9f
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 21:37:14 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -15,10 +15,17 @@ function locationLoadSuccess(pos){
     // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
     map.panTo(currentPos);
 
-    var locPosition = new kakao.maps.LatLng(pos.coords.latitude, pos.coords.longitude)
-       
-      // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition);
+ // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function(position) {
+        
+        var lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+        
+        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+        
+        // 마커와 인포윈도우를 표시합니다
+        displayMarker(locPosition, message);
 
 };
 
