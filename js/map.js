205b3702da63b6b6a03247ef51dbdeaedcafd89b
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 20:23:09 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,61 +1,27 @@
+  $(function() {        
+        // Geolocation API에 액세스할 수 있는지를 확인
+        if (navigator.geolocation) {
+            //위치 정보를 정기적으로 얻기
+            var id = navigator.geolocation.watchPosition(
+                    function(pos) {
+                        $('#latitude').html(pos.coords.latitude);     // 위도 
+                        $('#longitude').html(pos.coords.longitude); // 경도 
+                    });
+            
+        } else {
+            alert("이 브라우저에서는 현재 위치 불러오기가 지원되지 않습니다.")
+        }
+        
+    });
+
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(37.5643882, 126.9653482), // 지도의 중심좌표
+        center: new kakao.maps.LatLng($('#latitude'), $('#longitude')), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-// HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-if (navigator.geolocation) {
-    
-    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    navigator.geolocation.getCurrentPosition(function(position) {
-        
-        var lat = position.coords.latitude, // 위도
-            lon = position.coords.longitude; // 경도
-        
-        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-            message = '<div class="search">현재 계신곳!</div>'
-        
-        // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition, message);
-            
-      });
-    
-} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-    
-    var locPosition = new kakao.maps.LatLng(37.5643882, 126.9653482),    
-        message = '위치를 정확히 불러올수 없어요 ...ㅠㅠㅠ'
-        
-    displayMarker(locPosition, message);
-  }
-
-
-// 지도에 마커와 인포윈도우를 표시하는 함수입니다
-function displayMarker(locPosition, message) {
-
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({  
-        map: map, 
-        position: locPosition
-    }); 
-    
-    var iwContent = message, // 인포윈도우에 표시할 내용
-        iwRemoveable = true;
-
-    // 인포윈도우를 생성합니다
-    var infowindow = new kakao.maps.InfoWindow({
-        content : iwContent,
-        removable : iwRemoveable
-    });
-    
-    // 인포윈도우를 마커위에 표시합니다 
-    infowindow.open(map, marker);
-    
-    // 지도 중심좌표를 접속위치로 변경합니다
-    map.setCenter(locPosition);      
-}    
 
 // 서울
 function Seoul() {
