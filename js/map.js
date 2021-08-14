6557c019c34f1c765b6b660d95fdf7af02aed7cb
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 20:34:41 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,48 +1,38 @@
-// GPS START
-var gps_use = null; //gps의 사용가능 여부
-var gps_lat = null; // 위도
-var gps_lng = null; // 경도
-var gps_position; // gps 위치 객체
-
-gps_check();
-// gps가 이용가능한지 체크하는 함수이며, 이용가능하다면 show location 함수를 불러온다.
-// 만약 작동되지 않는다면 경고창을 띄우고, 에러가 있다면 errorHandler 함수를 불러온다.
-// timeout을 통해 시간제한을 둔다.
-function gps_check(){
-    if (navigator.geolocation) {
-        var options = {timeout:60000};
-        navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
-    } else {
-        alert("GPS_추적이 불가합니다.");
-        gps_use = false;
-    }
-}
-
-
-// gps 이용 가능 시, 위도와 경도를 반환하는 showlocation함수.
-function showLocation(position) {
-    gps_use = true;
-    gps_lat = position.coords.latitude;
-    gps_lng = position.coords.longitude;
-}
-
-
-// error발생 시 에러의 종류를 알려주는 함수.
-function errorHandler(error) {
-    if(error.code == 1) {
-        alert("접근차단");
-    } else if( err.code == 2) {
-        alert("위치를 반환할 수 없습니다.");
-    }
-    gps_use = false;
-}
-// GPS END
-
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(gps_lat, gps_lng), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.56646, 126.98121), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+        mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
     };
+
+// 지도를 생성한다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+function locationLoadSuccess(pos){
+    // 현재 위치 받아오기
+    var currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+
+    // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
+    map.panTo(currentPos);
+
+    // 마커 생성
+    var marker = new kakao.maps.Marker({
+        position: currentPos
+    });
+
+    // 기존에 마커가 있다면 제거
+    marker.setMap(null);
+    marker.setMap(map);
+};
+
+function locationLoadError(pos){
+    alert('위치 정보를 가져오는데 실패했습니다.');
+};
+
+// 위치 가져오기 버튼 클릭시
+function getCurrentPosBtn(){
+    navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
+};
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 // 서울
