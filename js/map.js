fd143c655254b11fa100d974e07ea229da4ed196
easycastle <sean34418290@gmail.com>
2021-08-15 05:16:20 +0900

    a

--- js/map.js
+++ js/map.js
@@ -261,19 +261,20 @@ function searchPlaces() {
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
     
             // 결과값으로 받은 위치를 마커로 표시합니다
-            var marker = new kakao.maps.Marker({
-                map: map,
-                position: coords
-            });
+            // var marker = new kakao.maps.Marker({
+            //     map: map,
+            //     position: coords
+            // });
     
-//                 // 인포윈도우로 장소에 대한 설명을 표시합니다
-//                 var infowindow = new kakao.maps.InfoWindow({
-//                     content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
-//                 });
-//                 infowindow.open(map, marker);
+                // // 인포윈도우로 장소에 대한 설명을 표시합니다
+                // var infowindow = new kakao.maps.InfoWindow({
+                //     content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
+                // });
+                // infowindow.open(map, marker);
     
             // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-            map.setCenter(coords);
+            // map.setCenter(coords);
+            map.panTo(coords);
         } 
     });
 }
