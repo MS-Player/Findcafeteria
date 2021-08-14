4ef65b1cf81eb779484481f2dc7030a965038ccd
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 20:29:10 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -14,15 +14,6 @@ function gps_tracking(){
     }
 }
 
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng('#latitude', '#longitude'), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
-
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-
 // 서울
 function Seoul() {
     // 이동할 위도 경도 위치를 생성합니다 
