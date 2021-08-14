8378274ebf5d623365ca31e1722829cbf6290954
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 22:09:29 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,59 +1,18 @@
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        function(location){
 
-            //succ - 유저가 허용버튼을 클릭하여 값을 가져올 수 있는 상태
-            var lat = location.coords.latitude;
-            var lon = location.coords.longitude;
-        },
-        function(error){
-            alert('위치 정보를 가져오는데 실패했습니다.');
-
-       }
-    );
-}
-else {
-    alert('위치 정보를 가져오는데 실패했습니다.');
-}
-
+navigator.geolocation.getCurrentPosition(function(position) {
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
-        center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(position.coords.latitude + ", " + position.coords.longitude), // 지도의 중심좌표
         level: 3, // 지도의 확대 레벨
         mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
 };
-
+});
 // 지도를 생성한다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
 
-function locationLoadSuccess(pos){
-    // 현재 위치 받아오기
-    var currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
-
-    // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
-    map.panTo(currentPos);
-
-    // 마커 생성
-    var marker = new kakao.maps.Marker({
-        position: currentPos
-    });
-
-    // 기존에 마커가 있다면 제거
-    marker.setMap(null);
-    marker.setMap(map);
-};
-
-function locationLoadError(pos){
-    alert('위치 정보를 가져오는데 실패했습니다.');
-};
-
-// 위치 가져오기 버튼 클릭시
-function getCurrentPosBtn(){
-    navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
-}; 
 
 // 서울
 function Seoul() {
