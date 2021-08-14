ca0f9288bb93c0e450eda5ebf459cfdfdb6a1786
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 20:30:02 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,3 +1,42 @@
+// GPS START
+var gps_use = null; //gps의 사용가능 여부
+var gps_lat = null; // 위도
+var gps_lng = null; // 경도
+var gps_position; // gps 위치 객체
+
+gps_check();
+// gps가 이용가능한지 체크하는 함수이며, 이용가능하다면 show location 함수를 불러온다.
+// 만약 작동되지 않는다면 경고창을 띄우고, 에러가 있다면 errorHandler 함수를 불러온다.
+// timeout을 통해 시간제한을 둔다.
+function gps_check(){
+    if (navigator.geolocation) {
+        var options = {timeout:60000};
+        navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+    } else {
+        alert("GPS_추적이 불가합니다.");
+        gps_use = false;
+    }
+}
+
+
+// gps 이용 가능 시, 위도와 경도를 반환하는 showlocation함수.
+function showLocation(position) {
+    gps_use = true;
+    gps_lat = position.coords.latitude;
+    gps_lng = position.coords.longitude;
+}
+
+
+// error발생 시 에러의 종류를 알려주는 함수.
+function errorHandler(error) {
+    if(error.code == 1) {
+        alert("접근차단");
+    } else if( err.code == 2) {
+        alert("위치를 반환할 수 없습니다.");
+    }
+    gps_use = false;
+}
+
 function gps_tracking(){
     if (gps_use) {
         map.panTo(new kakao.maps.LatLng(gps_lat,gps_lng));
@@ -13,6 +52,7 @@ function gps_tracking(){
       gps_check();
     }
 }
+// GPS END
 
 // 서울
 function Seoul() {
