133e89ea7f682e987a4c8c47c0d536ce318de986
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 20:24:18 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -16,7 +16,7 @@
 
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng($('#latitude'), $('#longitude')), // 지도의 중심좌표
+        center: new kakao.maps.LatLng('#latitude', '#longitude'), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
