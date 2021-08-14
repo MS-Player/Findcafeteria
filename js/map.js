3dad7e42b7ee78e546cc3003a7dac7a09bed5276
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 22:54:25 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -32,12 +32,20 @@ function locationLoadSuccess(pos){
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);
     
-    // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
-    var iwContent = '<div style="padding:5px;">Hello World!</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
+    var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
     // 인포윈도우를 생성합니다
     var infowindow = new kakao.maps.InfoWindow({
-    content : iwContent
+    content : iwContent,
+    removable : iwRemoveable
+});
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+      // 마커 위에 인포윈도우를 표시합니다
+      infowindow.open(map, marker);  
 });
 
 // 마커에 마우스오버 이벤트를 등록합니다
