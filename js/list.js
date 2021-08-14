8ac3827f53c26bbb34955295b832a8a6f78e102a
천상의나무 <ajb8533296@gmail.com>
2021-08-15 03:55:38 +0900

    a

--- js/list.js
+++ js/list.js
@@ -1,7 +1,58 @@
-function getLocation() {
+function list_result() {
+    const obj = document.getElementById("cafeteria_list");
+    let lists = document.createElement("div")
+
     if (navigator.geolocation) { // GPS를 지원하면
         navigator.geolocation.getCurrentPosition(function(position) {
-            return position.coords.latitude, position.coords.longitude;
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            let latitude_ok = new Array();
+            let longitude_ok = new Array();
+
+            // 위도 처리
+            for (let cafeteria of json_data) {
+                let latitude_cal = latitude - Number(latitude);
+                if (Math.abs(latitude_cal) <= 0.001) {
+                    latitude_ok.push(cafeteria);
+                }
+            };
+        
+            //경도 처리
+            for (let cafeteria of latitude_ok) {
+                let longitude_cal = longitude - Number(longitude);
+                if (Math.abs(longitude_cal) <= 0.001) {
+                    longitude_ok.push(cafeteria);
+                }
+            }
+
+            if (longitude_ok.length === 0) {
+                lists.innerHTML = '<center><h1 class="no_cafeteria">회원님의 근처엔<br>무료 급식소가 없네요 :(</h1></center>'
+                obj.appendChild(lists);
+            } else {
+                // 리스트 처리
+                for (let cafeteria of longitude_ok) {                
+                    lists.innerHTML = '<div class="cafeteria_list_box">'+
+                        '   <span class="cafeteria_name">'+ cafeteria.fcltyNm._text +'</span><br>'+
+                        '   <span class="cafeteria_address">' + cafeteria.rdnmadr._text + '</span><br><br>'+
+                        '   <span class="topic_txt">지급대상</span><br>'+
+                        '   <span class="target_person">' + cafeteria.mlsvTrget._text + '</span><br><br>'+
+                        '   <span class="topic_txt">영업시간</span><br>'+
+                        '   <div style="display: flex;">'+
+                        '       <div>'+
+                        '           <span class="time">' + cafeteria.mlsvTime._text + '</span><br>'+
+                        '           <span class="day_of_the_week">' + cafeteria.mlsvDate._text + '</span>'+
+                        '       </div>'+
+                        '       <a class="call" href="tel:' + cafeteria.phoneNumber._text + '">'+
+                        '           <div style="display: flex; align-items: center; justify-content: center;">'+
+                        '               <i class="fa fa-phone" aria-hidden="true" style="color: #ffffff; font-size: 30px;"></i>'+
+                        '           </div>'+
+                        '       </a>'+
+                        '   </div>'+
+                        '</div>'
+                    obj.appendChild(lists);
+                }
+            }
         }, function(error) {
             console.error(error);
         }, {
@@ -13,59 +64,15 @@ function getLocation() {
         alert('GPS가 차단되었거나 찾을 수 없습니다.');
     }
 }
-
-const convert = require('xml-js');
-const request = require('request');
-const fs = require('fs')
-
-var requestUrl = `http://api.data.go.kr/openapi/tn_pubr_public_free_mlsv_api?serviceKey=w4WgRe0atRPd8sTRA1ca%2BWFFWFmvzG7mwOKRIoNtnUucJyKMjEzCfngxtf9BZaE5SYgG849atzzWO1L7mOyi5g%3D%3D&type=xml&numOfRows=100000`
-var xmlFile = 'xml_data.xml'
-var jsonFile = 'json_data.json'
-
-function givemejson() {
-    request.get(requestUrl, (err,res,body) =>{
-        if(err){
-            console.log(`err => ${err}`)
-        }
-        else {
-            if(res.statusCode == 200){
-                var xmlResult = body // 기존 xml 파일
-                fs.writeFile(xmlFile, xmlResult, 'utf8', function(error){
-                    console.log('complete write xml')
-                });
-
-                var jsonResult = convert.xml2json(xmlResult, {compact: true, spaces: 4}); // json 변환 결과
-                return jsonResult
-                //fs.writeFile(jsonFile, jsonResult, 'utf8', function(error){
-                //    console.log('complete write json')
-                //});
-            }
-        }
-    })
-}
-
+/*
 function list_result() {
-    
-    let json_cafeteria_data = givemejson();
-
-
-
-    //let json_cafeteria_data = readTextFile("json_data.json", function(text) {
-    //    JSON.parse(text);
-    //});
-    console.log(json_cafeteria_data)
-
-    let latitude, longitude = getLocation();
-    //let json_cafeteria_data = JSON.parse(json_data);
-    //let latitude = showPosition.coords.latitude;
-    //let longitude = showPosition.coords.longitude;
 
     let latitude_ok = new Array();
     let longitude_ok = new Array();
 
     // 위도 처리
-    for (let cafeteria of json_cafeteria_data) {
-        let latitude_cal = latitude - Number(cafeteria.latitude._text);
+    for (let cafeteria of json_data) {
+        let latitude_cal = latitude - Number(latitude);
         if (Math.abs(latitude_cal) <= 0.001) {
             latitude_ok.push(cafeteria);
         }
@@ -73,7 +80,7 @@ function list_result() {
 
     //경도 처리
     for (let cafeteria of latitude_ok) {
-        let longitude_cal = longitude - Number(cafeteria.longitude._text);
+        let longitude_cal = longitude - Number(longitude);
         if (Math.abs(longitude_cal) <= 0.001) {
             longitude_ok.push(cafeteria);
         }
@@ -114,3 +121,4 @@ function list_result() {
         }
     }
 }
+*/
