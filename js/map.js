ec7cf78071c2912c71fbf0da8ee4847f015875ed
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 21:06:34 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,13 +1,3 @@
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-    mapOption = {
-        center: new kakao.maps.LatLng(37.56646, 126.98121), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
-        mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
-    };
-
-// 지도를 생성한다
-var map = new kakao.maps.Map(mapContainer, mapOption);
-
 function locationLoadSuccess(pos){
     // 현재 위치 받아오기
     var currentPos = new kakao.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
@@ -28,6 +18,17 @@ function locationLoadSuccess(pos){
 function locationLoadError(pos){
     alert('위치 정보를 가져오는데 실패했습니다.');
 };
+
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(pos.coords.latitude, pos.coords.longitude), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+        mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
+    };
+
+// 지도를 생성한다
+var map = new kakao.maps.Map(mapContainer, mapOption);
 
 // 위치 가져오기 버튼 클릭시
 function getCurrentPosBtn(){
