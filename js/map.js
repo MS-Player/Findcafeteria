d44ec3c586798463cfa3f920ac5943ada99d59c8
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 22:00:32 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -3,17 +3,6 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         center: new kakao.maps.LatLng(37.56646, 126.98121), // 지도의 중심좌표
         level: 3, // 지도의 확대 레벨
         mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
-};
-
-// 지도를 생성한다
-var map = new kakao.maps.Map(mapContainer, mapOption);
-
-
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-    mapOption = {
-        center: new kakao.maps.LatLng(37.56646, 126.98121), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
-        mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
     };
 
 // 지도를 생성한다
