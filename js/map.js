9bac540d03c01a9b68674c9e6e01b1adbee527b3
easycastle <sean34418290@gmail.com>
2021-08-15 05:14:05 +0900

    a

--- js/map.js
+++ js/map.js
@@ -247,39 +247,35 @@ function zoomOut() {
 // 키워드 검색을 요청하는 함수입니다
 function searchPlaces() {
 
-    if (window.event.keyCode == 13) {
-        var keyword = document.getElementByClassName('search').value;
+    if (!keyword.replace(/^\s+|\s+$/g, '')) {
+        alert('키워드를 입력해주세요!');
+        return false;
+    }
 
-        if (!keyword.replace(/^\s+|\s+$/g, '')) {
-            alert('키워드를 입력해주세요!');
-            return false;
-        }
-
-        // 주소로 좌표를 검색합니다
-        geocoder.addressSearch(keyword, function(result, status) {
-        
-            // 정상적으로 검색이 완료됐으면 
-            if (status === kakao.maps.services.Status.OK) {
-        
-                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-        
-                // 결과값으로 받은 위치를 마커로 표시합니다
-                var marker = new kakao.maps.Marker({
-                    map: map,
-                    position: coords
-                });
-        
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(keyword, function(result, status) {
+    
+        // 정상적으로 검색이 완료됐으면 
+        if (status === kakao.maps.services.Status.OK) {
+    
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+    
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
+    
 //                 // 인포윈도우로 장소에 대한 설명을 표시합니다
 //                 var infowindow = new kakao.maps.InfoWindow({
 //                     content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
 //                 });
 //                 infowindow.open(map, marker);
-        
-                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-                map.setCenter(coords);
-            } 
-        });
-    }
+    
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+        } 
+    });
 }
 
 // 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
