108a03b7e4410b10103e541d49bac0ee5be29771
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 21:32:27 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -26,6 +26,31 @@ function locationLoadError(pos){
     alert('위치 정보를 가져오는데 실패했습니다.');
 };
 
+// 지도에 마커와 인포윈도우를 표시하는 함수입니다
+function displayMarker(locPosition, message) {
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({  
+        map: map, 
+        position: locPosition
+    }); 
+    
+    var iwContent = message, // 인포윈도우에 표시할 내용
+        iwRemoveable = true;
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        content : iwContent,
+        removable : iwRemoveable
+    });
+    
+    // 인포윈도우를 마커위에 표시합니다 
+    infowindow.open(map, marker);
+    
+    // 지도 중심좌표를 접속위치로 변경합니다
+    map.setCenter(locPosition);      
+}    
+
 // 위치 가져오기 버튼 클릭시
 function getCurrentPosBtn(){
     navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
