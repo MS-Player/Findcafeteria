4608f07ab0a95974a5ad5613555cbf9723866f68
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 23:15:06 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -193,7 +193,7 @@ function Chnn() {
 // 제주
 function Jeju() {
     // 이동할 위도 경도 위치를 생성합니다 
-    var moveLatLon = new kakao.maps.LatLng(33.6632403, 126.2928464);
+    var moveLatLon = new kakao.maps.LatLng(33.4889923, 126.4973843);
     
     // 지도 중심을 부드럽게 이동시킵니다
     // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
