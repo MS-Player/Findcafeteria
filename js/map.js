9f82b794b3174a6969f23992e1e5680de274aae5
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 22:26:00 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -16,7 +16,7 @@ function locationLoadSuccess(pos){
     map.panTo(currentPos);
 
     var imageSrc = 'https://ms-player.github.io/icons/mylocation.svg', // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageSize = new kakao.maps.Size(128, 128), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
