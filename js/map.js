93b8870cdac4c7371a64c84f8b23cde640f5d4bb
Byuong Guk Kim <kimka2013@naver.com>
2021-08-14 22:15:18 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -8,6 +8,14 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 생성한다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
+var imageSrc = 'https://ms-player.github.io/icons/mylocation.svg', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+      
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+
 function locationLoadSuccess(pos){
     // 현재 위치 받아오기
     var currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
@@ -18,6 +26,7 @@ function locationLoadSuccess(pos){
     // 마커 생성
     var marker = new kakao.maps.Marker({
         position: currentPos
+        image: markerImage // 마커이미지 설정 
     });
 
     // 기존에 마커가 있다면 제거
