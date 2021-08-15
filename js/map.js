7b84030948d6a42b6e7f2fbb7b9541638325de19
easycastle <sean34418290@gmail.com>
2021-08-15 09:48:37 +0900

    a

--- js/map.js
+++ js/map.js
@@ -251,61 +251,37 @@ function zoomOut() {
 // 키워드 검색을 요청하는 함수입니다
 function searchPlaces() {
     
-    // var keyword = document.getElementsByClassName('search')[0].value;
+    var keyword = document.getElementsByClassName('search')[0].value;
     
-    // if (!keyword.replace(/^\s+|\s+$/g, '')) {
-    //     alert('키워드를 입력해주세요!');
-    //     return false;
-    // }
+    if (!keyword.replace(/^\s+|\s+$/g, '')) {
+        alert('키워드를 입력해주세요!');
+        return false;
+    }
     
-    // // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
-    // ps.keywordSearch(keyword, placesSearchCB); 
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(keyword, function(result, status) {
 
-    // 마커가 표시될 위치입니다 
-    var markerPosition  = new kakao.maps.LatLng(35.5390653, 129.3103105); 
+    // 정상적으로 검색이 완료됐으면 
+        if (status === kakao.maps.services.Status.OK) {
 
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        position: markerPosition
-    });
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
-}
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
 
-// 키워드 검색 완료 시 호출되는 콜백함수 입니다
-function placesSearchCB(data, status) {
-    if (status === kakao.maps.services.Status.OK) {
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            var infowindow = new kakao.maps.InfoWindow({
+                content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
+            });
+            infowindow.open(map, marker);
 
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        // LatLngBounds 객체에 좌표를 추가합니다
-        var bounds = new kakao.maps.LatLngBounds();
-        
-        for (var i=0; i<data.length; i++) {
-            displayMarker(data[i]);    
-            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-        }       
-        
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-        map.setBounds(bounds);
-    } 
-}
-
-// 지도에 마커를 표시하는 함수입니다
-function displayMarker(place) {
-    
-    // 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(place.y, place.x) 
-    });
-
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-        infowindow.open(map, marker);
-    });
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+        } 
+    });    
 }
 
 // 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
