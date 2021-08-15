d69e89e7a2d3e36d7e8ca4ec100d431f7a5e84ee
천상의나무 <ajb8533296@gmail.com>
2021-08-15 18:35:53 +0900

    ㅁ

--- js/map.js
+++ js/map.js
@@ -326,7 +326,7 @@ for (var i = 0; i < positions.length; i ++) {
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
-        //position: positions[i].latlng, // 마커를 표시할 위치
+        position: positions[i].latlng, // 마커를 표시할 위치
         //title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
         image : markerImage, // 마커 이미지
         clickable: true
@@ -338,10 +338,11 @@ for (var i = 0; i < positions.length; i ++) {
 
     // 인포윈도우를 생성합니다
     var infowindow = new kakao.maps.InfoWindow({
-    content : iwContent,
-    removable : iwRemoveable
-})};
+        content : iwContent,
+        removable : iwRemoveable
+    });
 
-kakao.maps.event.addListener(map, 'click', function(iwContent) {        
-    infowindow.open(map, marker)
-});
+    kakao.maps.event.addListener(map, 'click', function() {        
+        infowindow.open(map, marker)
+    })
+};
