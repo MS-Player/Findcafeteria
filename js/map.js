fce0658929873f2478173fa07247a6288e83e726
천상의나무 <ajb8533296@gmail.com>
2021-08-15 18:14:29 +0900

    ㅁ

--- js/map.js
+++ js/map.js
@@ -328,7 +328,8 @@ for (var i = 0; i < positions.length; i ++) {
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng, // 마커를 표시할 위치
         title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        image : markerImage // 마커 이미지 
+        image : markerImage, // 마커 이미지 
+        clickable: true
     });
 
     // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
