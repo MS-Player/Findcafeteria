9191076c01302ed05500ae8533488172dd2fa90d
easycastle <sean34418290@gmail.com>
2021-08-15 12:03:38 +0900

    last

--- js/map.js
+++ js/map.js
@@ -11,6 +11,9 @@ var map = new kakao.maps.Map(mapContainer, mapOption);
 // 장소 검색 객체를 생성합니다
 var ps = new kakao.maps.services.Places();
 
+// 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
 // 내 위치 마커 START
 function locationLoadSuccess(pos){
     // 현재 위치 받아오기
@@ -55,7 +58,6 @@ var geocoder = new kakao.maps.services.Geocoder();
 
 // 서울
 function Seoul() {
-    alert('안뇽 서울!!!'); 
     // 이동할 위도 경도 위치를 생성합니다 
     var moveLatLon = new kakao.maps.LatLng(37.5659436, 126.9778288);
     
