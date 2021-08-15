a52cfe02ffba1e44ae6ff2613356db2182da24d1
천상의나무 <ajb8533296@gmail.com>
2021-08-15 19:02:12 +0900

    ㅁ

--- js/map.js
+++ js/map.js
@@ -314,7 +314,7 @@ for (let cafeteria of json_data) {
 
 // 마커 이미지의 이미지 주소입니다
 var imageSrc = "../icons/pin_notenadbled.svg"; 
-/*
+
 for (let cafeteria of positions) {
 
     // 마커 이미지의 이미지 크기 입니다
@@ -342,39 +342,7 @@ for (let cafeteria of positions) {
         removable : iwRemoveable
     });
 
-    kakao.maps.event.addListener(marker, 'click', infowindow);
-}
-*/
-
-for (var i = 0; i < positions.length; i ++) {
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        map: map, // 마커를 표시할 지도
-        position: positions[i].latlng // 마커의 위치
-    });
-
-    // 마커에 표시할 인포윈도우를 생성합니다 
-    var infowindow = new kakao.maps.InfoWindow({
-        content: positions[i].content // 인포윈도우에 표시할 내용
-    });
-
-    // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-    // 이벤트 리스너로는 클로저를 만들어 등록합니다 
-    // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-    kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-    kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
-}
-
-// 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
-function makeOverListener(map, marker, infowindow) {
-    return function() {
+    kakao.maps.event.addListener(map, 'click', function() {        
         infowindow.open(map, marker);
-    };
-}
-
-// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
-function makeOutListener(infowindow) {
-    return function() {
-        infowindow.close();
-    };
-}
+    });
+};
