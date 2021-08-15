8938c34841d32b5473da45149ac4fd3a81eb8a15
easycastle <sean34418290@gmail.com>
2021-08-15 17:46:15 +0900

    a

--- js/map.js
+++ js/map.js
@@ -251,23 +251,6 @@ function zoomOut() {
     displayLevel(); 
 }
 
-// 지도에 마커를 표시하는 함수입니다
-function displayMarker(place) {
-    
-    // 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(place.y, place.x) 
-    });
-
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-        infowindow.open(map, marker);
-    });
-}
-
 // 키워드 검색을 요청하는 함수입니다
 function searchPlaces() {
     
@@ -287,7 +270,6 @@ function searchPlaces() {
             var bounds = new kakao.maps.LatLngBounds();
 
             for (var i=0; i<data.length; i++) {
-                displayMarker(data[i]);    
                 bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
             }       
 
