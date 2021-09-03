29ea11b4d94069f8d86ee013d6ed59eb12797512
천상의나무 <ajb8533296@gmail.com>
2021-09-03 16:11:31 +0900

    코드 정리

--- js/map.js
+++ js/map.js
@@ -5,8 +5,8 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
     };
 
-// 지도를 생성합니다    
-var map = new kakao.maps.Map(mapContainer, mapOption); 
+// 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
 
 // 장소 검색 객체를 생성합니다
 var ps = new kakao.maps.services.Places();
@@ -19,13 +19,13 @@ function locationLoadSuccess(pos){
     // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
     map.panTo(currentPos);
 
-    var imageSrc = 'icons/mylocation.svg', // 마커이미지의 주소입니다    
+    var imageSrc = 'icons/mylocation.svg', // 마커이미지의 주소입니다
     imageSize = new kakao.maps.Size(128, 128), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-      
+
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-    markerPosition = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude); // 마커가 표시될 위치입니다 
+    markerPosition = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude); // 마커가 표시될 위치입니다
     
     // 마커 생성
     var marker = new kakao.maps.Marker({
@@ -36,7 +36,6 @@ function locationLoadSuccess(pos){
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(null);
     marker.setMap(map);
-    
 };
 
 function locationLoadError(pos){
@@ -232,25 +231,19 @@ function zoomIn() {
     
     // 지도를 1레벨 내립니다 (지도가 확대됩니다)
     map.setLevel(level - 1);
-    
-    // 지도 레벨을 표시합니다
-    displayLevel();
 }    
 
-function zoomOut() {    
+function zoomOut() {
     // 현재 지도의 레벨을 얻어옵니다
-    var level = map.getLevel(); 
-    
+    var level = map.getLevel();
+
     // 지도를 1레벨 올립니다 (지도가 축소됩니다)
     map.setLevel(level + 1);
-    
-    // 지도 레벨을 표시합니다
-    displayLevel();
 }
 
 // 키워드 검색을 요청하는 함수입니다
 function searchPlaces() {
-    
+
     var keyword = document.getElementsByClassName('search')[0].value;
 
     if (!keyword.replace(/^\s+|\s+$/g, '')) {
@@ -268,17 +261,17 @@ function searchPlaces() {
 
             for (var i=0; i<data.length; i++) {
                 bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-            }       
+            }
 
             // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
             map.setBounds(bounds);
-        } 
-    });     
+        }
+    });
 
 }
 
 // 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
-function setMapType() { 
+function setMapType() {
     const satelite_grey = document.getElementById('satelite_grey');
     const satelite_green = document.getElementById('satelite_green');
     if (satelite_grey.style.display === 'none') {
@@ -299,7 +292,7 @@ function setMapType() {
 let positions = new Array();
 
 for (let cafeteria of json_data) {
-    try {    
+    try {
         positions.push(
             {
                 fcltyNm: cafeteria.fcltyNm._text,
@@ -315,16 +308,16 @@ for (let cafeteria of json_data) {
 };
 
 // 마커 이미지의 이미지 주소입니다
-var imageSrc = "icons/pin_notenadbled.svg"; 
+var imageSrc = "icons/pin_notenadbled.svg";
 var cafeteria_list_box = document.getElementById('cafeteria_list_box')
 for (var i = 0; i < positions.length; i ++) {
-    
+
     // 마커 이미지의 이미지 크기 입니다
-    var imageSize = new kakao.maps.Size(128, 128); 
-    
-    // 마커 이미지를 생성합니다    
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
-    
+    var imageSize = new kakao.maps.Size(128, 128);
+
+    // 마커 이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
     // 마커를 생성합니다
     displayMarker(positions[i]);
 }
@@ -334,7 +327,7 @@ function displayMarker(positions) {
         map: map,
         position: positions.latlng, // 마커를 표시할 위치
         // title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        image : markerImage, // 마커 이미지 
+        image : markerImage, // 마커 이미지
         // clickable: true
     });
 
